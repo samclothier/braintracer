69628f89f3953acd0ea33d4f77881e1621793729
Sam <sam@exabytegames.com>
2022-10-03 15:42:07 +0100

    Improved support for multiple channels in automatic bt.bat file

--- bt.bat
+++ bt.bat
@@ -7,56 +7,57 @@ set folder=braintracer
 if not exist %folder% mkdir %folder%
 if not exist %folder%\downsampled_data mkdir %folder%\downsampled_data
 if not exist %folder%\cellfinder mkdir %folder%\cellfinder
-if not exist %folder%\ground_truth mkdir %folder%\ground_truth
+if not exist %folder%\figures mkdir %folder%\figures
 if not exist %folder%\structures.csv copy %USERPROFILE%\.brainglobe\allen_mouse_10um_v1.2\structures.csv %folder%
 
 set /p dataset=Dataset name: 
-set /p s=Signal channel: 
+set /p s_chs=Signal channels (include spaces for multiple): 
 set /p b=Background channel: 
-
-:choice
-set /p c=Cellfinder already run? 
-if /i "%c%" EQU "Y" goto :no_run
-if /i "%c%" EQU "N" goto :run
-goto :choice
-
-:run
+set /p np=Trained network path: 
+set /p nn=Trained network nickname: 
 set /p res=Resolution (z x y; eg. 5 2 2): 
-ECHO Running cellfinder...
-cd %dataset%
-if not exist cellfinder_%s% (
-cellfinder -s %s% -b %b% -o cellfinder_%s% -v %res% --orientation psl --threshold 35 --atlas allen_mouse_10um --batch-size 128
-) else (
-ECHO Results already exist for signal channel %s%!
+set /p t=Threshold: 
+set /p atres=Atlas resolution (um; eg. 10): 
+
+set /p reverse=Also perform cell detection in background channel against the signal channel (only works for one signal channel)? 
+
+(for %%s in (%s_chs%) do (
+	ECHO Running cellfinder...
+	cd %dataset%
+	if [%np%] == [] ( :: if no trained network
+		if not exist cellfinder_%s% (
+			cellfinder -s %%s -b %b% -o cellfinder_%%s -v %res% --orientation psr --threshold %t% --soma-diameter 20 --atlas allen_mouse_%atres%um --batch-size 64
+		) else (
+			ECHO Results already exist for signal channel %%s!
+	)) else (
+		if not exist cellfinder_%s%_%nn% (
+			cellfinder -s %%s -b %b% -o cellfinder_%%s_%nn% -v %res% --orientation psr --threshold %t% --soma-diameter 20 --atlas allen_mouse_%atres%um --batch-size 64 --trained-model %np%
+	))
+	cd ..
+	ECHO Copying results...
+	copy %dataset%\cellfinder_%%s_%nn%\registration\downsampled_standard_channel_0.tiff %folder%\downsampled_data\reg_%dataset%_%%s.tiff
+	copy %dataset%\cellfinder_%%s_%nn%\registration\downsampled_standard.tiff %folder%\downsampled_data\reg_%dataset%_%b%.tiff
+	copy %dataset%\cellfinder_%%s_%nn%\analysis\all_points.csv %folder%\cellfinder\cells_%dataset%_%nn%_%%s.csv
+))
+:: do the same but without copying the downsampled images (they are identical to the forward run)
+if reverse EQU "Y" (
+	ECHO Running cellfinder...
+	cd %dataset%
+	if [%np%] == [] ( :: if no trained network
+		if not exist cellfinder_%b% (
+			cellfinder -s %b% -b %s_chs% -o cellfinder_%b% -v %res% --orientation psr --threshold %t% --soma-diameter 20 --atlas allen_mouse_%atres%um --batch-size 64
+		) else (
+			ECHO Results already exist for background channel %b%!
+	)) else (
+		if not exist cellfinder_%b%_%n_n% (
+			cellfinder -s %b% -b %s_chs% -o cellfinder_%b%_%nn% -v %res% --orientation psr --threshold %t% --soma-diameter 20 --atlas allen_mouse_%atres%um --batch-size 64 --trained-model %np%
+	))
+	cd ..
+	ECHO Copying results...
+	copy %dataset%\cellfinder_%b%\analysis\all_points.csv %folder%\cellfinder\cells_%dataset%_%nn%_%b%.csv
 )
-cd ..
-ECHO Copying results...
-copy %dataset%\cellfinder_%s%\registration\downsampled_standard.tiff %folder%\downsampled_data\reg_%s%_%dataset%.tiff
-copy %dataset%\cellfinder_%s%\analysis\all_points.csv %folder%\cellfinder\cells_%s%_%dataset%.csv
 
-ECHO Running cellfinder...
-cd %dataset%
-if not exist cellfinder_%b% (
-cellfinder -s %b% -b %s% -o cellfinder_%b% -v %res% --orientation psl --threshold 35 --atlas allen_mouse_10um --batch-size 128
-) else (
-ECHO Results already exist for background channel %b%!
-)
-cd ..
-ECHO Copying results...
-copy %dataset%\cellfinder_%b%\registration\downsampled_standard.tiff %folder%\downsampled_data\reg_%b%_%dataset%.tiff
-copy %dataset%\cellfinder_%b%\analysis\all_points.csv %folder%\cellfinder\cells_%b%_%dataset%.csv
-
-goto :end
-
-:no_run
-ECHO Copying cellfinder output...
-copy %dataset%\cellfinder_%s%\registration\downsampled_standard.tiff %folder%\downsampled_data\reg_%s%_%dataset%.tiff
-copy %dataset%\cellfinder_%s%\analysis\all_points.csv %folder%\cellfinder\cells_%s%_%dataset%.csv
-copy %dataset%\cellfinder_%b%\registration\downsampled_standard.tiff %folder%\downsampled_data\reg_%b%_%dataset%.tiff
-copy %dataset%\cellfinder_%b%\analysis\all_points.csv %folder%\cellfinder\cells_%b%_%dataset%.csv
-
-:end
 ECHO Done!
 ECHO [35mOpen bt_visualiser.ipynb in Jupyter to view the results.[0m
 PAUSE
-jupyter notebook
+jupyter-lab
